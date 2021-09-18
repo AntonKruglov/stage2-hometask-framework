e3ceebbfe4eabffa8f5772215184a2774932ed22
Anton Kruglov <76586992+AntonKruglov@users.noreply.github.com>
2021-09-18 23:47:31 +0300

    Update googleCloudCalculationResultsPage.js
    some locator changes

--- test/pageobjects/googleCloudCalculationResultsPage.js
+++ test/pageobjects/googleCloudCalculationResultsPage.js
@@ -5,19 +5,19 @@ class GoogleCloudCalculationResultsPage extends BasePage {
 
 
     get vmClassField() { return $('md-list-item.md-1-line:nth-child(8)') };
-    get instanceTypeField() { return $('#compute md-list md-list-item:nth-child(10)') };
+    get instanceTypeField() { return $('#input_70') };
     get regionField() { return $('#compute md-list md-list-item:nth-child(2)') };
-    get localSsdField() { return $('md-list-item:nth-child(14) div.md-list-item-text.ng-binding.cpc-cart-multiline') };
+    get localSsdField() { return $('#select_option_424 > .md-text') };
     get commitmentTermField() { return $('#compute md-list md-list-item.md-1-line.md-no-proxy.ng-scope') };
     get estimatedCostField() { return $('h2.md-title:nth-child(2) b:nth-child(1)') };
     get emailEstimateBtn() { return $('#email_quote') };
     get emailField() { return $('//*[@ng-model="emailQuote.user.email"]') };
     get sendEmailBtn() { return $('[aria-label="Send Email"]') };
-    get randomEmailBtn() { return $('a.hlink:nth-child(4)') };
+    get randomEmailBtn() { return $('#webmail') };
     get copyBtn() { return $('//*[@id="cprnd"]') };
-    get yopmailCheckInboxBtn() { return $('button.md:nth-child(3) i:nth-child(1)') };
+    get yopmailCheckInboxBtn() { return $('.md:nth-child(3)') };
     get refreshBtn() { return $('#refresh') };
-    get estimatedCostFromGoogle() { return $('//*[@id="mail"]/div/div/table/tbody/tr[2]/td/table/tbody/tr[2]/td[2]') };
+    get estimatedCostFromGoogle() { return $('//*[@id="mail"]') };
     get getTextBtn() { return $('//*[text()="Text"]') };
 
 
